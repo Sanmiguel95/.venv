17a465d5500f34d4426a785f7e50ed0076198d52
Camilo Sanmiguel <camilosanmiguel81@gmail.com>
2022-08-18 20:02:12 -0500

    prometheus

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 import requests
 
 app = FastAPI()
@@ -16,3 +18,5 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+Instrumentator().instrument(app).expose(app)
