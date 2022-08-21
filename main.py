4af3eedbdb1352696358c32bb0adad79eca7e514
Camilo Sanmiguel <camilosanmiguel81@gmail.com>
2022-08-21 14:35:55 -0500

    service1 test2

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from typing import Union
-
-from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
-
+from fastapi import FastAPI, status, Response
 import requests
-
+from prometheus_fastapi_instrumentator import Instrumentator
 import logging.config
+from pydantic import BaseModel
 
+# setup loggers
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
-# get root logger
-logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
-                                      # This will get the root logger since no logger in the configuration has this name.
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -19,11 +15,13 @@ class ResponseData(BaseModel):
     idUsuario: str
     internalId: int = 0
 
+Instrumentator().instrument(app).expose(app)
+
 @app.get("/infoUsers/{idUsuario}", response_model=ResponseData)
 def read_root(idUsuario: str):
     logger.debug("idUsuario recibido: " + idUsuario)
     
-    url = 'https://63025538c6dda4f287b7ee62.mockapi.io/infoUsers/service1/'
+    url = 'https://63016ffbe71700618a3866e4.mockapi.io/users'
     responseData = ResponseData(idUsuario= idUsuario)
     
     requestResult = requests.get(url + "?usuario=" + idUsuario, timeout=5)
@@ -33,5 +31,3 @@ def read_root(idUsuario: str):
         return Response(content= responseData.json())
     else:
         return Response(status_code= status.HTTP_204_NO_CONTENT)
-
-Instrumentator().instrument(app).expose(app) 
